574d3d42af9eef1722f7f31bf7a3bfe38b742cab
gbotemi-tech <63763418+gbotemi-tech@users.noreply.github.com>
2020-04-16 03:20:36 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+function check(form) {
+    if ( form.email.value == "admin" && form.password.value == "admin") ? alert ("Login successfully"); window.location = "www.facebook.com" : alert("Attempt Login failed");
+}
